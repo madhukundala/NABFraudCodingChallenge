55ba1df4fbff73aa4b153d7e9ee2f0dba7a024be
Madhu Kundala <madhu.kundala@gmail.com>
2020-06-17 14:07:56 +1000

    added swagger configs

--- src/main/java/com/nab/finance/controller/PlayerRestController.java
+++ src/main/java/com/nab/finance/controller/PlayerRestController.java
@@ -3,7 +3,6 @@ package com.nab.finance.controller;
 import com.nab.finance.DTO.PlayerDTO;
 import com.nab.finance.domain.Player;
 import com.nab.finance.service.PlayerService;
-import com.nab.finance.service.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -39,15 +38,6 @@ public class PlayerRestController {
     public List<Player> getPlayers() {
         return playerService.listPlayers();
     }
-
-    /**
-     * @return
-     */
-    @GetMapping(value = "/loggedInPlayer", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Response<Player> getLoggedPlayer() {
-        return new Response<>(playerService.getLoggedUser(), Response.Status.CREATED);
-    }
-
 
 }
 
---
+++ src/main/java/com/nab/finance/controller/ServiceExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.nab.finance.controller;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class ServiceExceptionHandler {
+
+
+}
